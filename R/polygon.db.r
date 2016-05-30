f30c889a171a9cd55dfcc9cdaa42282b219e3b7e
Jae S Choi <choijaeseok@gmail.com>
2016-05-30 13:50:28 -0300

    deps

--- R/polygon.db.r
+++ R/polygon.db.r
@@ -6,8 +6,8 @@ polygon.db = function( DS="load", p=NULL, id=NULL, crs="+init=epsg:4326", plotma
   require( rgdal)
   require( sp)
   require(raster)
-  library(maps) 
-  library(mapdata)  # high resolution world coastlines/polygons  
+  library(maps)
+  library(mapdata)  # high resolution world coastlines/polygons
 
   if (DS=="map.background") {
     # load libraries to quickly map coastlines
@@ -26,17 +26,17 @@ polygon.db = function( DS="load", p=NULL, id=NULL, crs="+init=epsg:4326", plotma
     X = read.table (fn)
     colnames( X ) = c("lon", "lat" )
     if ( as.character(crs) != "+init=epsg:4326" ) {
-      YY = X 
+      YY = X
       coordinates(YY) = ~lon+lat
-      proj4string( YY) =  "+init=epsg:4326" 
-      Z = spTransform( YY, CRS(crs) ) 
+      proj4string( YY) =  "+init=epsg:4326"
+      Z = spTransform( YY, CRS(crs) )
       X = coordinates(Z)
-    } 
+    }
     if (plotmap) {
       if ( as.character(crs) != "+init=epsg:4326"  ) {
-        polygon.db( DS="map.background", p=p) 
+        polygon.db( DS="map.background", p=p)
       } else {
-        u = map( database="worldHires", regions=p$regions, xlim=p$xlim, ylim=p$ylim, fill=FALSE, plot=FALSE )  
+        u = map( database="worldHires", regions=p$regions, xlim=p$xlim, ylim=p$ylim, fill=FALSE, plot=FALSE )
         v = data.frame( cbind( u$x, u$y) )
         w = rgdal::project( as.matrix(v), proj=as.character(crs) )
         plot (w, pch=".", col="gray", xlab="Easting", ylab="Northing")
@@ -49,11 +49,11 @@ polygon.db = function( DS="load", p=NULL, id=NULL, crs="+init=epsg:4326", plotma
 
   if (DS=="create") {
     print( "Left mouse click and then to end right-mouse click (or [Esc] in Windows ) " )
-    polygon.db ( DS="map.background", p=p ) 
+    polygon.db ( DS="map.background", p=p )
     X = locator(type="o" )
     X = as.data.frame( X)
     colnames(X) = c("lon", "lat")
-    X = rbind(X, X[1,]) 
+    X = rbind(X, X[1,])
     lines (X)
     u = readline("If it looks good, type 'good' and [enter], otherwise interrrupt and start over.. " )
     save.filename = file.path( p$output.directory, p$output.filename )
@@ -62,7 +62,7 @@ polygon.db = function( DS="load", p=NULL, id=NULL, crs="+init=epsg:4326", plotma
       u = readline( "Filename (above) exists. Interrupt now otherwise it will be overwritten" )
     }
     write.table( X, file=save.filename )
-    return ("save completed")  
+    return ("save completed")
   }
 
 }
